0e1cab74da3d8aba3b23bbadc9aae00d5990b984
SlavaBaskov <bozhe4uk@gmail.com>
2025-01-31 21:29:41 +0300

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import {SETTINGS} from './settings';
+
+import express = require('express');
+const app = express();
+
+app.get('/api', (req, res) => {
+    res.json
+})
+
+app.listen(3000, () => {})
